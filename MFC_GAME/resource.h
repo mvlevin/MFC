8c37f43f8f476d96246c546ed9879393fcb3dd91
tita4k <tita4k@yandex.ru>
2015-03-27 00:47:15 +0400

    Create resource.h

---
+++ MFC_GAME/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Flappy_Bird.rc
+//
+#define IDR_MENU1                       101
+#define IDB_BITMAP1                     103
+#define IDB_BITMAP2                     107
+#define IDR_IDB_BITMAP21                108
+#define ID_NEWGAME                      40001
+#define ID_QUIT                         40002
+#define ID_HELP_FQS                     40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
